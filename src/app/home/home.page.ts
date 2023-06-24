906c8f79edc170cbf7919b7ae7f0d11c2641b794
Ricardo Caballero <ricpar123@gmail.com>
2023-06-24 10:49:34 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../interfaces/clientes';
+import { DatosService } from '../servicios/datos.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  qrData = '';
+  createdCode = '';
+  codeSplit = '';
+  clientes :Cliente []=  [];
+  cli = '';
+  
+
+  constructor( private qrDatos: DatosService) {}
+
+  ngOnInit(): void {
+    this.qrDatos.getClientes().subscribe( resp => {
+      this.clientes = resp['listaClientes'];
+      console.log('clientes: ', this.clientes);
+    });
+      
+  }
+
+  createCode(){
+    this.createdCode = this.cli + '\n' + this.qrData;
+    console.log(this.createdCode);
+    
+  }
+
 
 }
